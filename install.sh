72a509a92049b115f73f4b2244ab441725ff59be
keahi32 <50500100+keahi32@users.noreply.github.com>
2023-03-06 14:44:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ echo "Preparando archivos"
 echo " "
 echo "-------------------------------"
 mv /registrophp/* /app
-manolo=172.17.0.1
+manolo=172.17.0.2
 sed -i 's/^$HOST=".*"/$HOST="'$manolo'"/g' /app/config.php
 echo "-------------------------------"
 echo " "
